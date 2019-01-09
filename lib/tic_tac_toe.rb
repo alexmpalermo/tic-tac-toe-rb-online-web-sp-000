9714e778e6e236ff0420b8b0b0e2a195f70da471
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-09 21:42:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -132,6 +132,7 @@ def play(board)
   until over?(board)
   turn(board)
   if won?(board)
+    winner(board)
     puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
